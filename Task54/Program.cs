58043d5f7a031b543e632b007d5888e5b5fd1099
Tanya <wendetta_01@mail.ru>
2023-02-14 13:30:38 +0300

    Task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+var array = Generate2DArray(4, 4);
+Print2DArray(array);
+Console.WriteLine();
+FindSortLess(array);
+Print2DArray(array);
+
+void FindSortLess(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for(int n = 0; n < arr.GetLength(1) - 1; n++) 
+        {
+            if (arr[i, n] < arr[i, n + 1])
+            {
+            int more = arr[i, n + 1];
+            arr[i, n + 1] = arr[i, n];
+            arr[i, n] = more;
+            }
+        }
+    }
+  }
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(1, 10);
+            }
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
